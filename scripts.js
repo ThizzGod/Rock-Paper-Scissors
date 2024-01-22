3e71d8e75131b8e3a8744fd27814c05b7ac28f84
Max Barker <maxbarker26@gmail.com>
2024-01-22 10:10:11 -0900

    moved js to scripts file, add defer tag to scripts link, remove dead code

--- scripts.js
+++ scripts.js
@@ -1,2 +1,64 @@
-/* function getComputerChoice is designed to randomly generate the computer's choice of string */
-/* create variables for rock, paper, and scissors containing a number 0-2 */
+game()
+function game() {
+    let counter = 0
+    let compCounter = 0
+    function playerWinCounter() {
+        counter++
+        console.log(counter)
+    }
+    function computerWinCounter() {
+        compCounter++
+        console.log(compCounter)
+    }
+    for (playerWin = 0; playerWin < 5; playerWin++) {
+        playGame()
+        if (counter === 3) {
+            alert("You win game!");
+            game();
+        } else if (compCounter === 3) {
+            alert("You lose game!");
+            game();
+        }
+    }
+    console.log(playerWin)
+    function getComputerChoice(max) {
+        let computerChoice = Math.floor(Math.random() * max)
+        return computerChoice
+    }
+    function playGame() {
+        let computerChoice = getComputerChoice(3)
+        console.log(computerChoice)
+        let userChoice = prompt("Rock, paper, or scissors?").toLowerCase();
+
+        if (userChoice === "rock") { 
+            if (computerChoice === 2) {
+                alert("You win!");
+                playerWinCounter()
+            } else if (computerChoice === 1) {
+                alert("You lose!");
+                computerWinCounter()
+            } else { alert("Tie");
+                    playerWin-- ;}
+        } else if (userChoice === "paper") {
+            if (computerChoice === 2) {
+                alert("You lose!");
+                computerWinCounter()
+            } else if (computerChoice === 1) {
+                alert("Tie");
+                playerWin--;
+            } else {alert("You win!");
+                playerWinCounter()}
+        } else if (userChoice === 'scissors') {
+            if (computerChoice === 2) {
+                alert("Tie")
+                playerWin--;
+            } else if (computerChoice === 1) {
+                alert("You win!");
+                playerWinCounter()
+            } else { alert("You lose!");
+                computerWinCounter() }
+        } else {alert ("Invalid choice");
+                playerWin--;}
+        console.log(userChoice)
+    }
+}
